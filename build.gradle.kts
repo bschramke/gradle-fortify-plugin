8acdb5dbb85170648edceedd4dd320cf703053f1
Björn Schramke <bschramke@users.noreply.github.com>
2022-12-12 18:07:57 +0100

    Update gradle build scripts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,38 @@
 
-tasks.register("buildExampleHelloJava") {
-    group = "Build"
-    dependsOn(gradle.includedBuild("hello-java").task(":build"))
+afterEvaluate {
+    gradle.includedBuilds.forEach {
+        val relativePath = it.projectDir.relativeTo(rootDir).path
+        if(relativePath.startsWith("example")) {
+            registerExampleTasks(it)
+        }
+    }
 }
 
-tasks.register("buildExampleHelloKotlin") {
-    group = "Build"
-    dependsOn(gradle.includedBuild("hello-kotlin").task(":build"))
-}
+fun normalizeModuleName(name:String) = name.split('-')
+    .joinToString("") { it.capitalize() }
 
-tasks.register("fortifyExampleHelloJava") {
-    group = "Fortify"
-    dependsOn(gradle.includedBuild("hello-java").task(":fortify"))
-}
-
-tasks.register("fortifyExampleHelloKotlin") {
-    group = "Fortify"
-    dependsOn(gradle.includedBuild("hello-kotlin").task(":fortify"))
+fun registerExampleTasks(module: IncludedBuild) {
+    tasks {
+        val name = normalizeModuleName(module.name)
+        val buildTask = register("buildExample$name") {
+            group = "Build"
+            dependsOn(gradle.includedBuild(module.name).task(":build"))
+        }
+        val fortifyCleanTask = register("fortifyCleanExample$name") {
+            group = "Fortify"
+            dependsOn(gradle.includedBuild(module.name).task(":fortifyClean"))
+        }
+        val fortifyTranslateTask = register("fortifyTranslateExample$name") {
+            group = "Fortify"
+            dependsOn(gradle.includedBuild(module.name).task(":fortifyTranslateRun"))
+        }
+        val fortifyTask = register("fortifyExample$name") {
+            group = "Fortify"
+            dependsOn(gradle.includedBuild(module.name).task(":fortify"))
+        }
+        val printTask = register("tasksExample$name") {
+            group = "Help"
+            dependsOn(gradle.includedBuild(module.name).task(":tasks"))
+        }
+    }
 }
--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,19 +1,25 @@
 plugins {
     `java-gradle-plugin`
     `kotlin-dsl`
-    id("groovy")
-    id("com.gradle.plugin-publish") version "0.9.7"
+    id("com.gradle.plugin-publish") version "1.1.0"
 }
 
 gradlePlugin {
     plugins {
         create("fortifyPlugin") {
-             id = "com.github.fortify-gradle"
-             implementationClass = "com.github.bschramke.gradle.plugin.fortify.FortifyPlugin"
-             displayName = "Gradle Fortify Plugin"
-             description = "A Gradle plugin for building and publishing of Fortify artifacts for a static security analysis."
+            id = "com.github.fortify-gradle"
+            implementationClass = "com.github.bschramke.gradle.plugin.fortify.FortifyPlugin"
+            displayName = "Gradle Fortify Plugin"
+            description = "A Gradle plugin for building and publishing of Fortify artifacts for a static security analysis."
         }
     }
+}
+
+pluginBundle {
+     website = "https://github.com/bschramke/gradle-fortify-plugin"
+     vcsUrl = "https://github.com/bschramke/gradle-fortify-plugin"
+     description = "A Gradle plugin for building and publishing of Fortify artifacts for a static security analysis."
+     tags = listOf("fortify", "quality", "qa", "analysis", "security")
 }
 
 repositories {
@@ -21,30 +27,13 @@ repositories {
 }
 
 dependencies {
-    implementation(localGroovy())
     testImplementation(gradleTestKit())
     testImplementation("junit:junit:4.12")
 }
 
-// jar {
-//     from(projectDir) {
-//         include("LICENSE")
-//         into("META-INF")
-//     }
-// }
-
-// pluginBundle {
-//     website = "https://github.com/sw-samuraj/gradle-fortify-plugin"
-//     vcsUrl = "https://github.com/sw-samuraj/gradle-fortify-plugin"
-//     description = "Gradle plugin for running  Fortify static code analysis."
-//     tags = listOf("fortify", "quality", "qa", "analysis", "security")
-//
-//     plugins {
-//         create("fortifyPlugin") {
-//             id = "cz.swsamuraj.fortify"
-//             implementationClass = "cz.swsamuraj.gradle.fortify.FortifyPlugin"
-//             displayName = "Gradle Fortify Plugin"
-//             description = "A Gradle plugin for building and publishing of Fortify artifacts for a static security analysis."
-//         }
-//     }
-// }
+tasks.withType<org.gradle.jvm.tasks.Jar>() {
+    from(projectDir) {
+        include("LICENSE")
+        into("META-INF")
+    }
+}
